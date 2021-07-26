ef176b142032cfc8e48d92002618c30bb01ec742
caoanhquan <caoanhquan_qn@yahoo.com>
2021-07-26 14:26:02 +0700

    commit all changed code

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => console.log('DB connected successfully'));
 
 // START SERVER
 const port = process.env.PORT;
